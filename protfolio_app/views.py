1f5645815d203184b93da0d1f402de757a494f99
parashac <parashacharya139@gmail.com>
2025-07-20 19:03:44 +0545

    views and urls

--- protfolio_app/views.py
+++ protfolio_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from protfolio_app.models import Project
 # Create your views here.
+def protfolio(request):
+    project = Project.objects.all()
+    return render(request, "protfolio.html", {"project":project})
