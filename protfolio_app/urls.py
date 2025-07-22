b53d047c8f0a7237a1df1035db36473e5bb4a75d
parashac <parashacharya139@gmail.com>
2025-07-23 00:34:10 +0545

    Adding new project to protfolio

--- protfolio_app/urls.py
+++ protfolio_app/urls.py
@@ -4,6 +4,7 @@ from protfolio_app import views
 urlpatterns = [
     path("",views.protfolio,name="protfolio"),
     path("project/",views.project_list, name="project-list"),
+    path("project-create/", views.project_create, name="project-create"),
     path("project-update/<int:pk>/",views.project_update, name="project-update"),
     path("project-list/<int:pk>/", views.project_delete, name="project-delete")
 
--- protfolio_app/views.py
+++ protfolio_app/views.py
@@ -9,6 +9,19 @@ def protfolio(request):
 def project_list(request):
     projects = Project.objects.all()
     return render(request, 'project_list.html', {'projects': projects})
+
+def project_create(request):
+    if request.method == "GET":
+        form = ProjectForm()
+        return render(request, "project_create.html", {"form": form})
+    else:
+        form = ProjectForm(request.POST)
+
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.save()
+            return redirect("project-list")
+
 def project_update(request, pk):
     project = Project.objects.get(pk=pk)
     form= ProjectForm(request.POST, instance=project)
